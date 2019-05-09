78d9fbe29ffb4a69a403f216a341e5a05eca7b4e
韦粤信 <283940802@qq.com>
2019-05-09 23:56:23 +0800

    feat: change to A B C components

--- route/src/App.vue
+++ route/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <A/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import A from './components/A'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    A
   }
 }
 </script>
