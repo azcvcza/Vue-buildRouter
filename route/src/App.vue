7182b5e8938142e56b347a2ff9bb40e79b79857e
韦粤信 <283940802@qq.com>
2019-05-10 00:38:02 +0800

    feat: build basic structure and style

--- route/src/App.vue
+++ route/src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <A/>
+    <router-view/>
   </div>
 </template>
 
 <script>
 import A from './components/A'
-
+import B from './components/B'
+import C from './components/C'
+//import View from './components/View'
+const routes =[
+  {path:'/',component:A},
+  {path:'/A',component:A},
+  {path:'/B',component:B},
+  {path:'/C',component:C},  
+];
+const View ={
+  name:'router-view',
+  template:`<component :is=currentView></component>`,
+  data:()=>{
+    return {
+      currentView:A
+    }
+  }
+}
 export default {
   name: 'App',
   components: {
-    A
+    'router-view':View
   }
 }
 </script>
--- route/src/components/B.vue
+++ route/src/components/B.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ name }}</h1>
-    <p>{{type}}</p>
-    <p>{{weight}}</p>
-    <p>{{height}}</p>
-      
+    <div class="container">
+      <div class="item">|Type|<h3>{{type}}</h3></div>
+      <div class="item">|Weight|<h3>{{weight}}</h3></div>
+      <div class="item">|Height|<h3>{{height}}</h3></div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +27,16 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+.hello{
+  display: flex;
+  flex-direction: column;
+  justify-content: center
+}
+.container{
+  display:flex;
+  flex-direction: row;
+  justify-content: center
 }
 
 </style>
