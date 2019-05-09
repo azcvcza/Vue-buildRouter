34986c9a9048c5b813cc509d9927cc0c98b02bd7
韦粤信 <283940802@qq.com>
2019-05-10 01:16:22 +0800

    feat: build router-link feature

--- route/src/App.vue
+++ route/src/App.vue
@@ -1,65 +1,110 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div id="app">
+    <div class="container">
+      <img src="./assets/logo.png">
+			<div class="items">
+				<router-link to="/A"></router-link>
+			</div>
+			<div class="items">
+				<router-link to="/B"></router-link>
+			</div>
+			<div class="items">
+				<router-link to="/C"></router-link>
+			</div>
+		</div>
+		
+		<router-view/>
+		
+	</div>
 </template>
 
 <script>
-import A from './components/A'
-import B from './components/B'
-import C from './components/C'
-//import View from './components/View'
-const routes =[
-  {path:'/',component:A},
-  {path:'/A',component:A},
-  {path:'/B',component:B},
-  {path:'/C',component:C},  
-];
-const View ={
-  name:'router-view',
-  template:`<component :is=currentView></component>`,
-  data:()=>{
-    return {
-      currentView:{}
-    }
-  },
-  created() {
-    if (this.getRouteObject() === undefined) {
-      this.currentView = {
-        template: `
-          <h3 class="subtitle has-text-white">
-            Not Found :(. Pick a Pokémon from the list below!
-          </h3>
-        `
-      };
-    } else {
-      this.currentView = this.getRouteObject().component;
-    }
-  },
-  methods: {
-    getRouteObject() {
-      return routes.find(
-        route => route.path === window.location.pathname
-      );
-    }
-  }
-}
-export default {
-  name: 'App',
-  components: {
-    'router-view':View
-  }
-}
+	import A from "./components/A";
+	import B from "./components/B";
+	import C from "./components/C";
+	//import View from './components/View'
+	//View
+	const View = {
+		name: "router-view",
+		template: `<component :is=currentView></component>`,
+		data: () => {
+			return {
+				currentView: {}
+			};
+		},
+		created() {
+			if (this.getRouteObject() === undefined) {
+				this.currentView = {
+					template: `
+		          <h3 class="subtitle has-text-white">
+		            Not Found : no exact page
+		          </h3>
+		        `
+				};
+			} else {
+				this.currentView = this.getRouteObject().component;
+			}
+		},
+		methods: {
+			getRouteObject() {
+				return routes.find(route => route.path === window.location.pathname);
+			}
+		}
+	};
+	//
+	//Link
+	const Link = {
+		name: "router-link",
+
+		props: {
+			to: {
+				type: String,
+				required: true
+			}
+		},
+		template: `<a @click="navigate" :href="to">{{ to }}</a>`,
+		methods: {
+			navigate(evt) {
+				evt.preventDefault();
+				window.history.pushState(null, null, this.to);
+			}
+		}
+	};
+	//
+	const routes = [
+		{ path: "/", component: A },
+		{ path: "/A", component: A },
+		{ path: "/B", component: B },
+		{ path: "/C", component: C }
+	];
+
+	export default {
+		name: "App",
+		components: {
+			"router-view": View,
+			"router-link": Link
+		}
+	};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+ 
+  display: flex;
+  flex-direction: row;
+  justify-content: center
+
+}
+.container {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+  
 }
 </style>
